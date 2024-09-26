e96c7740a74d3c12622cb38c61c04c0a35c05e6f
Irodabonu <kamilovairodabonu@gmail.com>
2024-09-26 09:07:47 +0500

    Navbar and color changes for design of website

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-     path('', views.index, name='index'),
+    path('', views.mainpage, name='main'),
+    path('account/', views.accountpage, name='account'),
+    path('events/', views.eventspage, name='events'),
+    path('talktous/', views.talk_to_us, name='talktous'),
+    path('posts/', views.posts, name='posts'),
+    path('books/', views.books, name='books'),
+    path('archieve/', views.archieve, name='archieve'),
+    path('poets/', views.poets, name='poets'),
  ]
--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 
+def mainpage(request):
+    return render(request, 'mainpage.html')
 
-def index(request):
-    return render(request, 'index.html')
+def accountpage(request):
+    return render(request, 'accountpage.html')
+
+def eventspage(request):
+    return render(request, 'events_page.html', {'navbar_color': 'rgba(109, 162, 166, 1)'})
+
+def talk_to_us(request):
+    return render(request, 'talk_to_us_page.html')
+
+def posts(request):
+    return render(request, 'posts_page.html', {'navbar_color': 'rgba(101, 104, 104, 1)'})
+
+def books(request):
+    return render(request, 'books_page.html', {'navbar_color': 'rgba(101, 104, 104, 1)'})
+
+def archieve(request):
+    return render(request, 'archieve_page.html',{'navbar_color': 'rgba(148, 139, 122, 1)'})
+
+
+def poets(request):
+    return render(request, 'poets_page.html', {'navbar_color': 'rgba(101, 104, 104, 1)'})
+
+
+
+
+
